ec01811d2828d11883b6e6335296bbd7c5ed0769
George Jones <georgekeesjones@gmail.com>
2025-08-13 11:50:44 +0300

    lunch

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const logger = require('morgan')
+const cors = require('cors')
 
 const petRoutes = require('./routes/petRoutes')
 
@@ -16,6 +17,7 @@ mongoose.connection.on('connected', ()=>{
     console.log('connected to mongoDB')
 })
 
+app.use(cors({origin: 'http://localhost:5173'}))
 app.use(express.json())
 app.use(logger('dev'))
 app.use('/pets', petRoutes)
